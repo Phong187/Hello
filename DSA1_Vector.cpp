983c21b6d90035a0b8ab8c116b2fde668459bf3e
Phong187 <lamphong180702@gmail.com>
2025-02-05 15:31:33 +0700

    [Test] Test Vector iterator

--- DSA1_Vector.cpp
+++ DSA1_Vector.cpp
@@ -2,7 +2,9 @@
 using namespace std;
 int main(){
     vector<int> v = {1, 2, 3, 4, 5};
-    for(int i=0; i<v.size(); i++)
-        cout << v[i] <<' ';
+    v.push_back(7);
+    vector<int>:: iterator it = v.begin();
+    for(it = v.begin(); it != v.end(); it++)
+        cout << *it<< ' ';
      return 0;
 }
